6e31b4bc9a55c33b07ff6b4314d4be2f1701b360
liyu1029 <88327654+liyu1029@users.noreply.github.com>
2024-06-17 12:37:17 +0800

    Update DBSCAN.py

--- pages/DBSCAN.py
+++ pages/DBSCAN.py
@@ -14,7 +14,7 @@ from assets.plot_templates import map_layout
 
 # import dataframe and define variables
 parks_folder = "national_park_animal"
-df = pd.read_csv(f'{parks_folder}\\All_5parks.csv')
+df = pd.read_csv(f'{parks_folder}\All_5parks.csv')
 df.date = pd.to_datetime(df['date'], format='%Y%m%d')
 parks_name = ["墾丁", "雪霸", "太魯閣", "陽明山", "玉山"]
 weathers = ['PS01', 'TX01', 'RH01', 'WD01', 'WD02', 'WD07', 'WD08', 'PP01']
