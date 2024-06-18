c59e54aeefa14de4a625d86b150f28fd168574fe
liyu1029 <88327654+liyu1029@users.noreply.github.com>
2024-06-18 10:45:07 +0800

    Create pages_links.py

---
+++ assets/pages_links.py
@@ -0,0 +1,17 @@
+pages_links=[
+  # pages
+  ['name': 'Home', 'relative_path': 'https://dash-web-deploy.onrender.com/'],
+  ['name': 'Dbscan', 'relative_path': 'https://dash-web-deploy.onrender.com/dbscan'],
+  ['name': 'Map kmeans', 'relative_path': 'https://dash-web-deploy.onrender.com/dbscan'],
+
+  # pages1
+  ['name': 'Decision tree', 'relative_path': 'https://dash-web-deploy-1.onrender.com/decision-tree'],
+  ['name': 'Plots', 'relative_path': 'https://dash-web-deploy-1.onrender.com/plots'],
+
+  # pages2
+  ['name': 'Jaccard similarity', 'relative_path': 'https://dash-web-deploy-2.onrender.com/jaccard-similarity'],
+
+  # others
+  # ['name': 'Linear regression', 'relative_path': '/linear-regression'],
+  # ['name': 'Dynamic callbacks', 'relative_path': '/dynamic-callbacks'],
+]
